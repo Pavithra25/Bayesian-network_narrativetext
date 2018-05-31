86971ea1b3eb5d85dd770dc4f597c2344c36dadd
Pavithra25 <hkpavithra8@gmail.com>
2018-05-31 13:27:37 +0530

    Update narrative_text.py

--- narrative_text.py
+++ narrative_text.py
@@ -9,6 +9,9 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus.reader import ChunkedCorpusReader
 from collections import Counter
 from nltk.corpus import stopwords
+from pgmpy.models import BayesianModel
+from pgmpy.factors.discrete import TabularCPD
+
 def temp(tagged):
     first,snd = zip(*tagged1)
 path = '/users/PHK/python_file/panchatantra.txt'
@@ -72,27 +75,28 @@ p5 = Counter(tokens5)
 new =[]
 
 
-    panchatantra = BayesianModel([('wedge', 'monkey'),
-('monkey', 'merchant'),
-('merchant', 'mansons'),
-('merchant', 'carpenters'),('carpenters','mansons'),('carpenters','garden')])#,('garden','work'),('garden','morning'),('garden','break'),('mansons','garden'),('monkey','workers'),('monkey','site'),('workers','meals'),('workers','carpenters')])
-wedge = TabularCPD(
-variable='wedge',
-variable_card=2,
-values=[[0.5, 0.5]])
-#evidence=['monkey'],
-#evidence_card=[2])
-marchant = TabularCPD(
-variable='marchant',
-variable_card=2,
-values=[[0.5, 0.5]])
-#evidence=['monkey'],
-#evidence_card=[2])
-carpenters = TabularCPD(
-variable='carpenters',
-variable_card=2,
-values=[[0.5, 0.5]])
-#evidence=['wedge','merchant'],
-#evidence_card=[3,3])
-panchatantra.add_cpds(wedge,marchant,carpenters)
+model = BayesianModel([('D', 'G'), ('I', 'G'), ('G', 'L'), ('I', 'S')])
+cpd_d = TabularCPD(variable='D', variable_card=2, values=[[0.6, 0.4]])
+cpd_i = TabularCPD(variable='I', variable_card=2, values=[[0.7, 0.3]])
+cpd_g = TabularCPD(variable='G', variable_card=3, 
+                   values=[[0.3, 0.05, 0.9,  0.5],
+                           [0.4, 0.25, 0.08, 0.3],
+                           [0.3, 0.7,  0.02, 0.2]],
+                  evidence=['I', 'D'],
+                  evidence_card=[2, 2])
 
+cpd_l = TabularCPD(variable='L', variable_card=2, 
+                   values=[[0.1, 0.4, 0.99],
+                           [0.9, 0.6, 0.01]],
+                   evidence=['G'],
+                   evidence_card=[3])
+
+cpd_s = TabularCPD(variable='S', variable_card=2,
+                   values=[[0.95, 0.2],
+                           [0.05, 0.8]],
+                   evidence=['I'],
+                   evidence_card=[2])
+
+1234model.add_cpds(cpd_d, cpd_i, cpd_g, cpd_l, cpd_s)
+
+model.check_model()
